47566367b632fe457504f179ab8cd7a83af46005
abhinav6999 <89853717+abhinav6999@users.noreply.github.com>
2021-09-02 22:04:26 +0530

    Created new file

---
+++ Abhinav.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Abhinav{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
